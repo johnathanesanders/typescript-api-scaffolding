5162c6cbaef80845a2585a6c8cf385469d023cf8
Johnathan Sanders <johnathanesanders@outlook.com>
2019-09-11 07:33:05 -0500

    Dynamic Database Loading

--- src/common/database/database.ts
+++ src/common/database/database.ts
@@ -22,3 +22,45 @@ export enum DATATYPES {
     TINYINT = 'tinyint',
     UNIQUEIDENTIFIER = 'uniqueidentifier'
 }
+
+export interface IDatabaseAdapter {
+    executeTransaction<T>(transaction: PreparedStatement[]): Promise<T[]>,
+    query<T>(statement: PreparedStatement): Promise<any>
+}
+
+export class PreparedStatement {
+    private readonly query: string[];
+
+    constructor(query: string[], values: any[]) {
+        this.query = query;
+        this.values = values;
+    }
+
+    get text() {
+        return this.query.reduce((prev, curr, i) => prev + '$' + i + curr);
+    }
+
+    get values() {
+        return this.values;
+    }
+
+    set values(values: any[]) {
+        this.values = values;
+    }
+
+    append(input: PreparedStatement | string): PreparedStatement {
+        if (input instanceof PreparedStatement) {
+            this.query.push(...input.query);
+            this.query.push.apply(this.values, input.values);
+        } else {
+            this.query[this.query.length -1] += input;
+        }
+        return this;
+    }
+}
+
+export function Prepare(query: any, ..._values: any[]): PreparedStatement {
+    const queryLength = query.length;
+    const sliced = queryLength > 1 ? query.slice(0, queryLength - 1) : query;
+    return new PreparedStatement(sliced, Array.from(arguments).slice(1));
+}
