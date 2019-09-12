dd06540bf11f1410e9a3fe455f69ebec62a3ecaa
Johnathan Sanders <johnathanesanders@outlook.com>
2019-09-12 06:34:45 -0500

    process.env injection in environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+export const Environment: any = {
+    credentials: {
+        database: {
+            maxPoolSize: process.env.DATABASE_MAXPOOLSIZE,
+            name: process.env.DATABASE_NAME,
+            port: process.env.DATABASE_PORT,
+            protocol: process.env.DATABASE_PROTOCOL,
+            secret: process.env.DATABASE_SECRET,
+            server: process.env.DATABASE_SERVER,
+            timeout: process.env.DATABASE_TIMEOUT,
+            user: process.env.DATABASE_USER
+        }
+    },
+    environment: process.env.ENVIRONMENT,
+    error: {
+        debug: process.env.ERROR_DEBUG,
+        level: process.env.ERROR_LEVEL
+    },
+    namespace: process.env.NAMESPACE
+}
